bc49424b42a241bf40e4f98577421a811bf77102
Comingdonut <jamescastrejon12@gmail.com>
2017-09-03 12:01:47 -0600

    Added Background Image, Menu Label, and Menu Buttons

---
+++ Project Frontier/Project Frontier/MenuControllers/MainMenuController.swift
@@ -0,0 +1,20 @@
+//
+//  MainMenuController.swift
+//  Project Frontier
+//
+//  Created by James Castrejon on 9/3/17.
+//  Copyright © 2017 James Castrejon. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuController: UIViewController{
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
