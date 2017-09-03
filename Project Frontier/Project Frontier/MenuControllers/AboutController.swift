3f13cb672fe33fbdb1853e232f57c2f1045278cb
Comingdonut <jamescastrejon12@gmail.com>
2017-09-03 13:22:47 -0600

    Added classes for menu buttons

---
+++ Project Frontier/Project Frontier/MenuControllers/AboutController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutController.swift
+//  Project Frontier
+//
+//  Created by James Castrejon on 9/3/17.
+//  Copyright © 2017 James Castrejon. All rights reserved.
+//
+
+import UIKit
+
+class AboutController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
---
+++ Project Frontier/Project Frontier/MenuControllers/HelpController.swift
@@ -0,0 +1,21 @@
+//
+//  HelpController.swift
+//  Project Frontier
+//
+//  Created by James Castrejon on 9/3/17.
+//  Copyright © 2017 James Castrejon. All rights reserved.
+//
+
+import UIKit
+
+class HelpController: UIViewController{
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
