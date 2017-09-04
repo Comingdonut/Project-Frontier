93257a10e20b3fccb48c034b4f346be769abaee0
Comingdonut <jamescastrejon12@gmail.com>
2017-09-04 11:51:28 -0600

    Added Select Mode View, added Mode Information View, and added new images

---
+++ Project Frontier/Project Frontier/Controllers/InformationController.swift
@@ -0,0 +1,20 @@
+//
+//  InformationController.swift
+//  Project Frontier
+//
+//  Created by James Castrejon on 9/4/17.
+//  Copyright © 2017 James Castrejon. All rights reserved.
+//
+
+import UIKit
+
+class InformationController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ Project Frontier/Project Frontier/Controllers/SelectModeController.swift
@@ -0,0 +1,24 @@
+//
+//  SelectModeController.swift
+//  Project Frontier
+//
+//  Created by James Castrejon on 9/4/17.
+//  Copyright © 2017 James Castrejon. All rights reserved.
+//
+
+import UIKit
+
+class SelectModeController: UIViewController{
+    
+    let infoStr = "Information"
+    let selectStr = "Select a Mode"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
