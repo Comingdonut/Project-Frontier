c89481eaae9c05a999b33d99c1f6f2cc236d185f
Comingdonut <jamescastrejon12@gmail.com>
2017-09-03 20:46:37 -0600

    Added options view

---
+++ Project Frontier/Project Frontier/MenuControllers/OptionsController.swift
@@ -0,0 +1,21 @@
+//
+//  OptionsController.swift
+//  Project Frontier
+//
+//  Created by James Castrejon on 9/3/17.
+//  Copyright © 2017 James Castrejon. All rights reserved.
+//
+
+import UIKit
+
+class OptionsController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
