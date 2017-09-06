8bae22cf91ee431d58344cfe8b502268cf625d8c
Comingdonut <jamescastrejon12@gmail.com>
2017-09-06 15:33:43 -0600

    Updated ARController and setup skeleton for surface detection

--- Project Frontier/Project Frontier/Controllers/ARController.swift
+++ Project Frontier/Project Frontier/Controllers/ARController.swift
@@ -10,17 +10,42 @@ import UIKit
 import ARKit
 import SceneKit
 
-class ARController: UIViewController{
+class ARController: UIViewController, ARSCNViewDelegate{
     
     @IBOutlet weak var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Do any additional setup after the view is loaded, typically from a nib
-        let configuration = ARWorldTrackingConfiguration()
-        configuration.planeDetection = .horizontal
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Debug plane
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        
+        // Create a new scene
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        //sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
         sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
     }
     
     override func didReceiveMemoryWarning() {
@@ -28,6 +53,32 @@ class ARController: UIViewController{
         //Dispose of any resources that can be recreated
         
     }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    //Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+    
+    // MARK: - Outlets
     
     @IBAction func addBox(_ sender: Any) {
         let cubeNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
---
+++ Project Frontier/Project Frontier/Utilities/SurfacePlane.swift
@@ -0,0 +1,23 @@
+//
+//  SurfacePlane.swift
+//  Project Frontier
+//
+//  Created by James Castrejon on 9/6/17.
+//  Copyright © 2017 James Castrejon. All rights reserved.
+//
+
+import Foundation
+import ARKit
+import SceneKit
+
+class SurfacePlane: SCNNode{
+    
+    override init(){
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
