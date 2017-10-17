5750f37d2487db4be0f964dde3cadc94f9283d31
Comingdonut <jamescastrejon12@gmail.com>
2017-10-17 11:26:23 -0600

    Added orbits to planets and updated methods in subject

--- Project Frontier/Project Frontier/ARView/Suns/YellowSun.swift
+++ Project Frontier/Project Frontier/ARView/Suns/YellowSun.swift
@@ -27,54 +27,80 @@ class YellowSun: Subject {
             objects.append(node)
         }
         
-        initObject(0, "Medium Sun", 0.030, .sphere, texture: "yellowsun")
-        initObject(1, "Mercury", 0.010, .sphere, texture: "mercury")
-        initObject(2, "Venus", 0.013, .sphere, texture: "venus")
-        initObject(3, "Earth", 0.014, .sphere, texture: "earth")
-        initObject(4, "Mars", 0.012, .sphere, texture: "mars")
-        // TODO: Asteroid Belt Here
-        initObject(5, "Jupiter", 0.024, .sphere, texture: "jupiter")
-        initObject(6, "Saturn", 0.021, .sphere, texture: "saturn")
-        initObject(7, "Uranus", 0.019, .sphere, texture: "uranus")
-        initObject(8, "Neptune", 0.018, .sphere, texture: "neptune")
+        initObject(objects, 0, "Medium Sun", 0.030, .sphere, texture: "yellowsun")
+        initObject(objects, 1, "Mercury Helper", 0.010, .sphere, .clear)
+        initObject(objects, 2, "Venus Helper", 0.010, .sphere, .clear)
+        initObject(objects, 3, "Earth Helper", 0.010, .sphere, .clear)
+        initObject(objects, 4, "Mars Helper", 0.010, .sphere, .clear)
+        initObject(objects, 5, "Juptier Helper", 0.010, .sphere, .clear)
+        initObject(objects, 6, "Saturn Helper", 0.010, .sphere, .clear)
+        initObject(objects, 7, "Uranus Helper", 0.010, .sphere, .clear)
+        initObject(objects, 8, "Neptune Helper", 0.010, .sphere, .clear)
+        addChildrenNodes()
+//        initObject(1, "Mercury", 0.010, .sphere, texture: "mercury")
+//        initObject(2, "Venus", 0.013, .sphere, texture: "venus")
+//        initObject(3, "Earth", 0.014, .sphere, texture: "earth")
+//        initObject(4, "Mars", 0.012, .sphere, texture: "mars")
+//        // TODO: Asteroid Belt Here
+//        initObject(5, "Jupiter", 0.024, .sphere, texture: "jupiter")
+//        initObject(6, "Saturn", 0.021, .sphere, texture: "saturn")
+//        initObject(7, "Uranus", 0.019, .sphere, texture: "uranus")
+//        initObject(8, "Neptune", 0.018, .sphere, texture: "neptune")
     }
     
-    func initObject(_ index: Int, _ name: String, _ size: Float, _ geometry: Shape, _ color: Color) {
+    func initObject(_ objects: [ObjectNode], _ index: Int, _ name: String, _ size: Float, _ geometry: Shape, _ color: Color) {
         objects[index].setName(to: name)
         objects[index].setDimension(to: size)
         objects[index].setShape(geometry)
         objects[index].setColor(color)
     }
     
-    func initObject(_ index: Int, _ name: String, _ size: Float, _ geometry: Shape, _ r: Float, _ g: Float, _ b: Float, _ a: Float) {
+    func initObject(_ objects: [ObjectNode], _ index: Int, _ name: String, _ size: Float, _ geometry: Shape, _ r: Float, _ g: Float, _ b: Float, _ a: Float) {
         objects[index].setName(to: name)
         objects[index].setDimension(to: size)
         objects[index].setShape(geometry)
         objects[index].setColor(r, g, b, a)
     }
     
-    func initObject(_ index: Int, _ name: String, _ size: Float, _ geometry: Shape, texture: String) {
+    func initObject(_ objects: [ObjectNode], _ index: Int, _ name: String, _ size: Float, _ geometry: Shape, texture: String) {
         objects[index].setName(to: name)
         objects[index].setDimension(to: size)
         objects[index].setShape(geometry)
         objects[index].setTexture(to: texture)
     }
     
-    func initObject(_ index: Int, _ name: String, _ size: Float, _ geometry: Shape, image: String) {
+    func initObject(_ objects: [ObjectNode], _ index: Int, _ name: String, _ size: Float, _ geometry: Shape, image: String) {
         objects[index].setName(to: name)
         objects[index].setDimension(to: size)
         objects[index].setShape(geometry)
         objects[index].setImage(to: image)
     }
     
+    func addChildrenNodes() {
+        let planetNames: [String] = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+        let planetTextures: [String] = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+        let planetSize: [Float] = [0.010, 0.013, 0.014, 0.012, 0.024, 0.021, 0.019, 0.018]
+        var distance: Float = 0.078
+        let none: Float = 0.0
+        for x in stride(from: 1, to: size, by: 1) {
+            let node = ObjectNode()
+            node.setName(to: planetNames[x-1])
+            node.setDimension(to: planetSize[x-1])
+            node.setShape(.sphere)
+            node.setTexture(to: planetTextures[x-1])
+            node.setPosition(none, none, none, none, none, distance*node.multiplier)
+            objects[x].addChildNode(node)
+            distance+=0.048
+        }
+    }
+    
     func setObjectPositions(_ x: Float, _ y: Float, _ z: Float) {
-        var distance: Float = 0.030
+        let distance: Float = 0.030
         let offSet: Float = 0.20
         let none: Float = 0.0
         
         for obj in objects {
             obj.setPosition(x, y, z, none, offSet, distance*obj.multiplier)
-            distance+=0.048
         }
     }
     
@@ -86,34 +112,45 @@ class YellowSun: Subject {
     }
     
     func rotate() {
-//        anim.spin(objects[0], x: 0, y: 1, z: 0, d: Duration.medium)
-//        anim.spin(objects[1], x: 0, y: 1, z: 0, d: Duration.light)
-//        anim.spin(objects[2], x: 0, y: 1, z: 0, d: Duration.fast_fast)
-//        anim.spin(objects[3], x: 0, y: 1, z: 0, d: Duration.fast)
-//        anim.spin(objects[4], x: 0, y: 1, z: 0, d: Duration.fast_slow)
-//        anim.spin(objects[5], x: 0, y: 1, z: 0, d: Duration.medium)
-//        anim.spin(objects[6], x: 0, y: 1, z: 0, d: Duration.medium_slow)
-//        anim.spin(objects[7], x: 0, y: 1, z: 0, d: Duration.slow_fast)
-//        anim.spin(objects[8], x: 0, y: 1, z: 0, d: Duration.slow)
         let anim: Animation = Animation()
-        for x in stride(from: 0, to: 4, by: 1){
+        
+        for x in stride(from: 0, to: 9, by: 1){
             if x == 0 {
                 anim.infiniteRotate(objects[0], x: 0, y: 1, z: 0, d: Duration.medium)
-                anim.infiniteRotate(objects[1], x: 0, y: 1, z: 0, d: Duration.fast_fast)
+                anim.infiniteRotate(objects[1], x: 0, y: 1, z: 0, d: Duration.light)
             }
             else if x == 1 {
-                anim.infiniteRotate(objects[2], x: 0, y: -1, z: 0, d: Duration.light)
-                anim.infiniteRotate(objects[3], x: 0, y: 1, z: 0, d: Duration.medium)
+                anim.infiniteRotate(objects[2], x: 0, y: 1, z: 0, d: Duration.fast_fast)
+                anim.infiniteRotate(objects[3], x: 0, y: 1, z: 0, d: Duration.fast)
             }
             else if x == 2 {
-                anim.infiniteRotate(objects[4], x: 0, y: 1, z: 0, d: Duration.medium_fast)
-                anim.infiniteRotate(objects[5], x: 0, y: 1, z: 0, d: Duration.slow)
-                anim.infiniteRotate(objects[6], x: 0, y: 1, z: 0, d: Duration.slow_fast)
+                anim.infiniteRotate(objects[4], x: 0, y: 1, z: 0, d: Duration.fast_slow)
+                anim.infiniteRotate(objects[5], x: 0, y: 1, z: 0, d: Duration.medium)
             }
             else if x == 3 {
-                anim.infiniteRotate(objects[7], x: 0, y: 0, z: -1, d: Duration.medium_slow)
-                anim.spin(objects[7], x: 1.55, y: 0, z: 0, d: Duration.light)
-                anim.infiniteRotate(objects[8], x: 0, y: 1, z: 0, d: Duration.medium_slow)
+                anim.infiniteRotate(objects[6], x: 0, y: 1, z: 0, d: Duration.medium_slow)
+                anim.infiniteRotate(objects[7], x: 0, y: 1, z: 0, d: Duration.slow_fast)
+            }
+            else if x == 4 {
+                anim.infiniteRotate(objects[8], x: 0, y: 1, z: 0, d: Duration.slow)
+            }
+            else if x == 5 {
+                //anim.infiniteRotate(objects[0], x: 0, y: 1, z: 0, d: Duration.medium)
+                anim.infiniteRotate((objects[1].childNodes.first)!, x: 0, y: 1, z: 0, d: Duration.fast_fast)
+            }
+            else if x == 6 {
+                anim.infiniteRotate((objects[2].childNodes.first)!, x: 0, y: -1, z: 0, d: Duration.light)
+                anim.infiniteRotate((objects[3].childNodes.first)!, x: 0, y: 1, z: 0, d: Duration.medium)
+            }
+            else if x == 7 {
+                anim.infiniteRotate((objects[4].childNodes.first)!, x: 0, y: 1, z: 0, d: Duration.medium_fast)
+                anim.infiniteRotate((objects[5].childNodes.first)!, x: 0, y: 1, z: 0, d: Duration.slow)
+                anim.infiniteRotate((objects[6].childNodes.first)!, x: 0, y: 1, z: 0, d: Duration.slow_fast)
+            }
+            else if x == 8 {
+                anim.infiniteRotate((objects[7].childNodes.first)!, x: 0, y: 0, z: -1, d: Duration.medium_slow)
+                anim.spin((objects[7].childNodes.first)!, x: 1.55, y: 0, z: 0, d: Duration.light)
+                anim.infiniteRotate((objects[8].childNodes.first)!, x: 0, y: 1, z: 0, d: Duration.medium_slow)
             }
         }
     }
